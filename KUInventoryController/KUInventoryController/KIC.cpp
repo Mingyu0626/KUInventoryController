897a0768e14a3c05dc8b00f7b7c3f95bad854126
HyeseonBaek <bhs12087@naver.com>
2021-10-23 02:43:47 +0900

    혜선

--- KUInventoryController/KUInventoryController/KIC.cpp
+++ KUInventoryController/KUInventoryController/KIC.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main(void) {
 	printf("This is Mingyu's Branch");
 	printf("Branch Test 01");
-	printf("Branch Test 02bhs");
+	printf("Branch Test 02");
+	printf("น้วýผฑ");
 }
