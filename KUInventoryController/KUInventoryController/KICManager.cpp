ed007fbd257afac77995ecc65fefb8108808d3d6
oyj1028@konkuk.ac.kr <oyj1028@konkuk.ac.kr>
2021-10-23 20:36:01 +0900

    modify checkDate()

--- KUInventoryController/KUInventoryController/KICManager.cpp
+++ KUInventoryController/KUInventoryController/KICManager.cpp
@@ -22,7 +22,7 @@ void KICManager::start()
 string KICManager::checkDate(string date) 
 {
     int year = 0, month = 0, day = 0;
-    bool dateCheck = false, letterCheck = false;
+
     string a, b = "0";
     int num0, num1 = 0;
 
@@ -38,14 +38,24 @@ string KICManager::checkDate(string date)
     string i, j = "0";
     int num8, num9 = 0;
 
+    // dateCheck : 년도/월/일의 형식을 만족하고 범위 내에 있는지 확인
+    // letterCheck : 정해진 위치에 특수문자가 존재하는지 확인 
+    bool dateCheck = false, letterCheck = false;
+
+    // letterCheck 시 '.' 처리 위함 
+    string finalDate0 = "";
+    string finalDate1 = "";
+    string finalDate2 = "";
+
     while (true) {
         int z = 0;
 
+        // 선행 공백 예외 처리 
         if (date[0] == ' ') {
             cout << "잘못된 입력입니다. 선행 공백을 허용하지 않습니다." << endl;
             return "";
         }
-        // 011028 같이 숫자 6자리
+        // 6자리 입력 시(ex. 011028)
         if (date.length() == 6) {
             for (int k = 0; k < date.length(); k++) {
                 string tmp = "0";
@@ -132,7 +142,8 @@ string KICManager::checkDate(string date)
                 continue;
             }
         }
-        // 20201028    01-10-28
+
+        // 8 자리 입력 시(ex.20011028, 01-10-28)
         else if (date.length() == 8) {
             a = date[0];
             b = date[1];
@@ -143,7 +154,7 @@ string KICManager::checkDate(string date)
             g = date[6];
             h = date[7];
 
-            // 20011028
+            // 20011028의 경우
             if (date.find("-") == string::npos && date.find("/") == string::npos
                 && date.find(".") == string::npos && date.find("_") == string::npos) {
                 num0 = stoi(a);
@@ -187,128 +198,133 @@ string KICManager::checkDate(string date)
                     }
                 }
             }
-        }
-        else if (date.find("-") != string::npos || date.find("/") != string::npos || date.find(".") != string::npos || date.find("_") != string::npos) {
-            num0 = stoi(a);
-            num1 = stoi(b);
-            // num2 = stoi(c);
-            num3 = stoi(d);
-            num4 = stoi(e);
-            // num5 = stoi(f);
-            num6 = stoi(g);
-            num7 = stoi(h);
 
-            year = 10 * num0 + num1;
-            month = 10 * num3 + num4;
-            day = 10 * num6 + num7;
+            // 01-10-28의 경우 
+            else if (date.find("-") != string::npos || date.find("/") != string::npos || date.find(".") != string::npos || date.find("_") != string::npos) {
+                num0 = stoi(a);
+                num1 = stoi(b);
+                // num2 = stoi(c);
+                num3 = stoi(d);
+                num4 = stoi(e);
+                // num5 = stoi(f);
+                num6 = stoi(g);
+                num7 = stoi(h);
 
-            // dateCheck 
-            if (month >= 1 && month <= 12) {
-                if (month == 2) {
-                    if (day >= 1 && day <= 28) {
-                        dateCheck = true;
+                year = 10 * num0 + num1;
+                month = 10 * num3 + num4;
+                day = 10 * num6 + num7;
+
+                // dateCheck 
+                if (month >= 1 && month <= 12) {
+                    if (month == 2) {
+                        if (day >= 1 && day <= 28) {
+                            dateCheck = true;
+                        }
+                    }
+                    else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                        if (day >= 1 && day <= 30) {
+                            dateCheck = true;
+                        }
+                    }
+                    else {
+                        if (day >= 1 && day <= 31) {
+                            dateCheck = true;
+                        }
                     }
                 }
-                else if (month == 4 || month == 6 || month == 9 || month == 11) {
-                    if (day >= 1 && day <= 30) {
-                        dateCheck = true;
+
+                // letterCheck
+                if ((date[2] == '-') && (date[5] == '-'))
+                    letterCheck = true;
+                else if ((date[2] == '/') && (date[5] == '/'))
+                    letterCheck = true;
+                else if ((date[2] == '.') && (date[5] == '.'))
+                    letterCheck = true;
+                else if ((date[2] == '_') && (date[5] == '_'))
+                    letterCheck = true;
+
+                if (dateCheck && letterCheck) {
+                    if (date[2] == '-') {
+                        vector<string> finalDate = split(date, '-');
+                        date = finalDate[0] + finalDate[1] + finalDate[2];
+
+                        if (year >= 0 && year <= 21) {
+                            string n = "20";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                        else {
+                            string n = "19";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                    }
+                    else if (date[2] == '/') {
+                        vector<string> finalDate = split(date, '/');
+                        date = finalDate[0] + finalDate[1] + finalDate[2];
+
+                        if (year >= 0 && year <= 21) {
+                            string n = "20";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                        else {
+                            string n = "19";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                    }
+                    else if (date[2] == '.') {
+
+                        for (int k = 0; k < 2; k++) {
+                            finalDate0 += date[k];
+                        }
+                        for (int k = 3; k < 5; k++) {
+                            finalDate1 += date[k];
+                        }
+                        for (int k = 6; k < 8; k++) {
+                            finalDate2 += date[k];
+                        }
+                        date = finalDate0 + finalDate1 + finalDate2;
+
+                        if (year >= 0 && year <= 21) {
+                            string n = "20";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                        else {
+                            string n = "19";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                    }
+                    else if (date[2] == '_') {
+                        vector<string> finalDate = split(date, '_');
+                        date = finalDate[0] + finalDate[1] + finalDate[2];
+
+                        if (year >= 0 && year <= 21) {
+                            string n = "20";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
+                        else {
+                            string n = "19";
+                            finalCheck = true;
+                            return n.insert(2, date);
+                        }
                     }
                 }
                 else {
-                    if (day >= 1 && day <= 31) {
-                        dateCheck = true;
-                    }
-                }
-            }
-            else {
-                dateCheck = false;
-            }
-
-            // letterCheck
-            if ((date[2] == '-') && (date[5] == '-'))
-                letterCheck = true;
-            else if ((date[2] == '/') && (date[5] == '/'))
-                letterCheck = true;
-            else if ((date[2] == '.') && (date[5] == '.'))
-                letterCheck = true;
-            else if ((date[2] == '_') && (date[5] == '_'))
-                letterCheck = true;
-
-            if (dateCheck && letterCheck) {
-                if (date[2] == '-') {
-                    vector<string> finalDate = split(date, '-');
-                    date = finalDate[0] + finalDate[1] + finalDate[2];
-
-                    if (year >= 0 && year <= 21) {
-                        string n = "20";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                    else {
-                        string n = "19";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                }
-                else if (date[2] == '/') {
-                    vector<string> finalDate = split(date, '/');
-                    date = finalDate[0] + finalDate[1] + finalDate[2];
-
-                    if (year >= 0 && year <= 21) {
-                        string n = "20";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                    else {
-                        string n = "19";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                }
-                else if (date[2] == '.') {
-                    string finalDate;
-                    for (int k = 0; k < 2; k++) {
-                        finalDate[0] += date[k];
-                    }
-                    for (int k = 3; k < 5; k++) {
-                        finalDate[1] += date[k];
-                    }
-                    for (int k = 6; k < 8; k++) {
-                        finalDate[2] += date[k];
-                    }
-                    date = finalDate[0] + finalDate[1] + finalDate[2];
-
-                    if (year >= 0 && year <= 21) {
-                        string n = "20";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                    else {
-                        string n = "19";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                }
-                else if (date[2] == '_') {
-                    vector<string> finalDate = split(date, '_');
-                    date = finalDate[0] + finalDate[1] + finalDate[2];
-
-                    if (year >= 0 && year <= 21) {
-                        string n = "20";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
-                    else {
-                        string n = "19";
-                        finalCheck = true;
-                        return n.insert(2, date);
-                    }
+                    cout << "날짜를 올바르게 입력하세요." << endl;
+                    return "";
                 }
             }
         }
-        // 2021-03-29
+        
+        // 10 자리 입력 시 (ex.2021-03-29)
         else if (date.length() == 10) {
-            if (date.find("-") != string::npos || date.find("/") != string::npos || date.find(".") != string::npos || date.find("_") != string::npos) {
+            if (date.find("-") != string::npos || date.find("/") != string::npos 
+                || date.find(".") != string::npos || date.find("_") != string::npos) {
                 a = date[0];
                 b = date[1];
                 c = date[2];
@@ -367,6 +383,8 @@ string KICManager::checkDate(string date)
                     letterCheck = true;
                 else if ((date[4] == '_') && (date[7] == '_'))
                     letterCheck = true;
+                else
+                    letterCheck = false;
 
                 if (dateCheck && letterCheck) {
                     if (date[4] == '-') {
@@ -382,17 +400,16 @@ string KICManager::checkDate(string date)
                         return date;
                     }
                     else if (date[4] == '.') {
-                        string finalDate;
                         for (int k = 0; k < 4; k++) {
-                            finalDate[0] += date[k];
+                            finalDate0 += date[k];
                         }
                         for (int k = 5; k < 7; k++) {
-                            finalDate[1] += date[k];
+                            finalDate1 += date[k];
                         }
                         for (int k = 8; k < 10; k++) {
-                            finalDate[2] += date[k];
+                            finalDate2 += date[k];
                         }
-                        date = finalDate[0] + finalDate[1] + finalDate[2];
+                        date = finalDate0 + finalDate1 + finalDate2;
                         finalCheck = true;
                         return date;
                     }
@@ -402,6 +419,10 @@ string KICManager::checkDate(string date)
                         finalCheck = true;
                         return date;
                     }
+                }
+                else {
+                    cout << "날짜를 올바르게 입력하세요." << endl;
+                    return "";
                 }
             }
         }
@@ -414,7 +435,8 @@ string KICManager::checkDate(string date)
 
 void KICManager::init()
 {
-    fstream fin("C:\\Users\\이하윤\\Source\\Repos\\Mingyu0626\\KUInventoryController\\KUInventoryController\\KUInventoryController\\source.txt");
+    //fstream fin("C:\\Users\\이하윤\\Source\\Repos\\Mingyu0626\\KUInventoryController\\KUInventoryController\\KUInventoryController\\source.txt");
+    fstream fin("source.txt");
 
     if (!fin.is_open()) {
         cerr << "파일 읽기 실패\n";
