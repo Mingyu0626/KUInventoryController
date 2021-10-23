0d282842813fbcde1389645e71f57de1330cdeff
Mingyu0626 <mg010626@naver.com>
2021-10-23 15:52:10 +0900

    add mant fuction and organize header

--- KUInventoryController/KUInventoryController/KICManager.cpp
+++ KUInventoryController/KUInventoryController/KICManager.cpp
@@ -1,46 +1,88 @@
 #include "KICManager.h"
 
-void KICManager::start()
+void KICManager::init()								// 텍스트 파일 읽어오기
 {
 }
 
-int KICManager::randomSales()
-{
-    return 0;
-}
-
-void KICManager::init()
+void KICManager::printMenu()						// 메인 화면의 메뉴 출력
 {
 }
 
-void KICManager::printMenu()
+void KICManager::noStockAlarm(KICProduct** kicp[])	// 재고 부족 알림
 {
 }
 
-void KICManager::addOrder()
+
+
+
+void KICManager::addOrder()		// 주문 추가
 {
 }
 
-void KICManager::discountPrice()
+
+void KICManager::searchProds()	// 제품 검색
 {
 }
 
-void KICManager::changePrice()
+void KICManager::sortStock()	// 정렬
 {
 }
 
-void KICManager::noStockAlarm()
+
+
+
+void KICManager::changePrice()	// 판매가 지정
 {
 }
 
-void KICManager::sortStock()
+void KICManager::discountProds(KICProduct** kicp[])	// 현재 할인중인 제품 출력
 {
 }
+
+void KICManager::discountReqProds(KICProduct** kicp[])	// 할인이 필요한 제품 판별
+{
+}
+
+void KICManager::selectDiscountProds(KICProduct** kicp[])	// 할인 제품 지정
+{
+}
+
+void KICManager::selectMarginRate(KICProduct** kicp[])	// 마진율 지정
+{
+}
+
+
+
+
+void KICManager::closingWork()	// 업무 마감
+{
+	//serachScrap(product);
+}
+
+void KICManager::searchScrap(KICProduct** kicp[])	// 폐기 제품을 판별 후 출력
+{
+}
+
+void KICManager::printFinance()	// 재정 상황 출력(당일 매출, 당일 순이익, 보유 자산)
+{
+}
+
+void KICManager::randomSV(KICProduct** kicp[])	// 판매량을 랜덤 알고리즘에 의해 지정
+{
+}
+
+
+
+
 
 KICManager::KICManager()
 {
 }
 
 KICManager::~KICManager()
+{
+}
+
+void KICManager::start()
 {
 }
